ce1a9123d5debf45b688c3dbefa8b8e8032385e6
paalamugan <paalamugan@gmail.com>
2023-03-13 01:23:05 +0530

    chore: remove test cases

--- test/video-converter.test.ts
+++
@@ -1,7 +0,0 @@
-import { combineMultipleVideoUrlIntoFile } from "../src";
-
-describe("video-converter", () => {
-  it("combineMultipleVideoUrlIntoFile", () => {
-    expect(combineMultipleVideoUrlIntoFile).toBeDefined();
-  });
-});
