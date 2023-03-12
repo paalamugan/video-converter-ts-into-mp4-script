1730911fa7eec3ab9642381d993187970f3ecf83
paalamugan <paalamugan@gmail.com>
2023-03-13 02:47:37 +0530

    chore: update ffprobe

--- src/lib/ffmpeg.ts
+++ src/lib/ffmpeg.ts
@@ -1,12 +1,10 @@
-import path from "path";
 import ffmpegPath from "ffmpeg-static";
-// import ffprobePath from "@ffprobe-installer/ffprobe";
+import ffprobePath from "@ffprobe-installer/ffprobe";
 import ffmpeg from "fluent-ffmpeg";
 import ffprobeClient from "ffprobe-client";
 
 const ffmpegPathUrl = process.env.FFMPEG_PATH || ffmpegPath;
-const ffprobePathUrl =
-  process.env.FFPROBE_PATH || path.join(__dirname, "..", "ffprobe-static", "bin", "ffprobe");
+const ffprobePathUrl = process.env.FFPROBE_PATH || ffprobePath.path;
 
 ffmpegPathUrl && ffmpeg.setFfmpegPath(ffmpegPathUrl);
 ffprobePathUrl && ffmpeg.setFfprobePath(ffprobePathUrl);
