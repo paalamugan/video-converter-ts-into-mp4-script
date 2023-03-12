9026824488478894cd10130c833c642e768ddbbe
paalamugan <paalamugan@gmail.com>
2023-03-13 03:30:09 +0530

    chore: fixed ffprobe issue

--- src/lib/ffmpeg.ts
+++ src/lib/ffmpeg.ts
@@ -1,18 +1,17 @@
 import ffmpegPath from "ffmpeg-static";
-import ffprobePath from "@ffprobe-installer/ffprobe";
+// import ffprobePath from "@ffprobe-installer/ffprobe";
 import ffmpeg from "fluent-ffmpeg";
 import ffprobeClient from "ffprobe-client";
 
 const ffmpegPathUrl = process.env.FFMPEG_PATH || ffmpegPath;
-const ffprobePathUrl = process.env.FFPROBE_PATH || ffprobePath.path;
+const ffprobePathUrl = process.env.FFPROBE_PATH || "";
 
 ffmpegPathUrl && ffmpeg.setFfmpegPath(ffmpegPathUrl);
 ffprobePathUrl && ffmpeg.setFfprobePath(ffprobePathUrl);
 
 const ffprobe = (inputPath: string) => {
-  return ffprobeClient(inputPath, {
-    path: ffprobePathUrl,
-  });
+  const config = ffprobePathUrl ? { path: ffprobePathUrl } : undefined;
+  return ffprobeClient(inputPath, config);
 };
 
 export { ffmpeg, ffprobe };
