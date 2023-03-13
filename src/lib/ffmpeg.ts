ff0e1dd4a10a64bc8da6189e51cf6843c0e547b5
paalamugan <paalamugan@gmail.com>
2023-03-14 01:10:18 +0530

    feat: added new ffprobe

--- src/lib/ffmpeg.ts
+++ src/lib/ffmpeg.ts
@@ -1,10 +1,10 @@
 import ffmpegPath from "ffmpeg-static";
-// import ffprobePath from "@ffprobe-installer/ffprobe";
+import ffprobePath from "@ffprobe-installer/ffprobe";
 import ffmpeg from "fluent-ffmpeg";
 import ffprobeClient from "ffprobe-client";
 
 const ffmpegPathUrl = process.env.FFMPEG_PATH || ffmpegPath;
-const ffprobePathUrl = process.env.FFPROBE_PATH || "";
+const ffprobePathUrl = process.env.FFPROBE_PATH || ffprobePath.path;
 
 ffmpegPathUrl && ffmpeg.setFfmpegPath(ffmpegPathUrl);
 ffprobePathUrl && ffmpeg.setFfprobePath(ffprobePathUrl);
