53dca7f6c9322cee9e9569ba39bb3c090d99ce46
paalamugan <paalamugan@gmail.com>
2023-03-14 22:26:04 +0530

    chore: added postinstall script to load ffmpeg and ffprobe

--- src/lib/ffmpeg.ts
+++ src/lib/ffmpeg.ts
@@ -1,10 +1,12 @@
-import ffmpegPath from "ffmpeg-static";
-import ffprobePath from "@ffprobe-installer/ffprobe";
 import ffmpeg from "fluent-ffmpeg";
 import ffprobeClient from "ffprobe-client";
+import path from "path";
+
+const ffmpegPath = path.resolve(__dirname, "..", "binaries", "ffmpeg");
+const ffprobePath = path.resolve(__dirname, "..", "binaries", "ffprobe");
 
 const ffmpegPathUrl = process.env.FFMPEG_PATH || ffmpegPath;
-const ffprobePathUrl = process.env.FFPROBE_PATH || ffprobePath.path;
+const ffprobePathUrl = process.env.FFPROBE_PATH || ffprobePath;
 
 ffmpegPathUrl && ffmpeg.setFfmpegPath(ffmpegPathUrl);
 ffprobePathUrl && ffmpeg.setFfprobePath(ffprobePathUrl);
